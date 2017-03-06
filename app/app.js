81041ff089f8eb6e3c1b3ae1b7adaa920a92708c
spacens <niksensoft@gmail.com>
2017-03-06 19:44:40 +0100

    merge

--- app/app.js
+++ app/app.js
@@ -3,14 +3,8 @@ import cors                 from 'cors';
 import morgan               from 'morgan';
 import bodyParser           from 'body-parser';
 import {errorHandler}       from './module-middlewares';
-
-<<<<<<< HEAD
 import restIdentity         from './rest-identity';
 import restUsers            from './rest-users';
-=======
-import ilRestIdentity       from './rest-identity';
-import ilRestUsers          from './rest-users';
->>>>>>> origin/master
 
 
 const app = express();
@@ -20,13 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-<<<<<<< HEAD
 restIdentity(app);
 restUsers(app);
-=======
-ilRestIdentity(app);
-ilRestUsers(app);
->>>>>>> origin/master
 
 app.use(errorHandler);
 
